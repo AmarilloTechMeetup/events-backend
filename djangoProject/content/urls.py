f112fe80ed7859c0821c3ef07bf3a9327babc975
omicron08 <30945115+omicron08@users.noreply.github.com>
2017-08-12 03:41:06 -0500

    Update urls.py

--- djangoProject/content/urls.py
+++ djangoProject/content/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = {
     url(r'^content$', views.list_content.as_view()),
     url(r'^category$', views.list_category.as_view()),
-    url(r'^event$', views.list_event.as_view()),
+    url(r'^event$', views.list_event),
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
