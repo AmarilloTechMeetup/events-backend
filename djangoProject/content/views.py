5fc60ca1b7aafb888f5c618b3bd33169898853ae
omicron08 <30945115+omicron08@users.noreply.github.com>
2017-08-12 04:26:24 -0500

    Update views.py

--- djangoProject/content/views.py
+++ djangoProject/content/views.py
@@ -16,11 +16,6 @@ class list_category(generics.ListAPIView):
     def get_queryset(self):
         return Content.objects.order_by('slug',).all()
 
-class list_event(generics.ListAPIView):
-    serializer_class = EventSerializer
-    def get_queryset(self):
-        return Event.objects.all()
-
 @api_view(['GET', 'POST'])
 def list_event(request):
     """
